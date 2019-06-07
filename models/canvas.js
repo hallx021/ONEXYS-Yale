3dd0f95348906114921af394972d77d77b03d71a
Tony  Fu <tonyfu@Tonys-MBP.hsd1.il.comcast.net>
2019-06-07 17:53:10 -0500

    change mongo url

--- models/canvas.js
+++ models/canvas.js
@@ -25,8 +25,9 @@ var notes_column_url = (courseID) => {
 
 var get_update_url = (courseID, callback) => {    
   getAdminRequest(notes_column_url(courseID),function(err,custom_columns){
-    var points_id = custom_columns.find(column => column.title='Notes').id;
-    var update_url = config.canvasURL + '/api/v1/courses/' + courseID + '/custom_gradebook_columns/' + points_id + '/data';
+    //var points_id = custom_columns.find(column => column.title='Notes').id;
+    //var update_url = config.canvasURL + '/api/v1/courses/' + courseID + '/custom_gradebook_columns/' + points_id + '/data';
+    var update_url =config.canvasURL + '/api/v1/courses/38082/custom_gradebook_columns/235/data/';
     callback(update_url);
   });
 }
